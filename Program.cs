b180b07003fc958da8d104c1c8ae5ac3d89aea63
Vladyslav Mykhailichenko <mykhailichenkovlad@gmail.com>
2023-03-27 19:59:46 +0300

    init project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_23_03_23_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
